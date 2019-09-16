bb1120e26a1741b0d9f0c87fac45a8a7eb2f923c
rahmaniali <arahmani@palo-it.com>
2019-09-16 14:31:55 +0200

    1st commit

---
+++ objectDetector/detector_settings.py
@@ -0,0 +1,10 @@
+# setting some directories and files
+YOLO_COCO_DIR = './yolo-coco'
+OBJECTS_CLASS_NAMES_FILE = '9k.names' #'coco.names'
+YOLO_CONFIG_FILE = 'yolo9000.cfg'
+YOLO_WEIGHTS_FILE = 'yolo9000.weights'
+
+IMAGE_TO_PROCESS = './test-images/soccer.jpg'
+
+CONFIDENCE = 0.5
+THRESHOLD = 0.3
---
+++ testgoogle.py
@@ -0,0 +1,9 @@
+import webbrowser
+import requests
+
+filePath = '/Users/ali/Desktop/CHAISE.jpg'
+searchUrl = 'http://www.google.hr/searchbyimage/upload'
+multipart = {'encoded_image': (filePath, open(filePath, 'rb')), 'image_content': ''}
+response = requests.post(searchUrl, files=multipart, allow_redirects=False)
+fetchUrl = response.headers['Location']
+webbrowser.open(fetchUrl)
